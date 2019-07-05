2e8d530957c539f837b00e7404eb5f01a0171ee5
sesprie <342560937@qq.com>
2019-07-05 14:02:14 +0800

    Initial commit

---
+++ springmvc_helloworld/src/main/java/com/xixi/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.xixi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("Hello Springmvc");
+        return "hello";
+    }
+}
+
+
+/*
+springmvc入门步骤
+1.编写controller
+2.在springmvc-servlet.xml中配置视图解析器，并扫描@Controller注解
+3.在web.xml中配置dispatcherServlet，指定springmvc-servlet.xml文件
+4.编写对应的jsp视图文件
+* */
