7d670299b3c6279c000cd9f654af958879285a69
sesprie <342560937@qq.com>
2019-07-09 22:29:19 +0800

    了解@ModelAttribute

--- springmvc_helloworld/src/main/java/com/xixi/controller/TestModelAttribute.java
+++ springmvc_helloworld/src/main/java/com/xixi/controller/TestModelAttribute.java
@@ -17,17 +17,20 @@ public class TestModelAttribute {
 
 
 
-    //@ModelAttribute会在每个目标方法执行之前被调用
+    //被@ModelAttribute修饰的方法会在每个目标方法执行之前被调用
     @ModelAttribute
     public void getUser(@RequestParam(value = "id", required = false) Integer id, Map<String, Object> map){
         if(id != null){
             //模拟从数据库中取数据
             User user = new User(1,"张三","123");
             System.out.println("从数据库获取的对象：" + user);
+            //在@ModelAttribute修饰的方法中，放入到Map中的键需要与目标方法的入参类型的第一个字母小写的字符串一致
             map.put("user", user);//在请求之前将user对象存入，在其覆盖时，没有的数据会保持原样
         }
     }
 
+
+    //在@ModelAttribute修饰的方法中，放入到Map中的键需要与目标方法的入参类型的第一个字母小写的字符串一致
     @RequestMapping(value = "/testModelAttribute")
     public String testModelAttribute(User user){
         //如果表单中给出了password的input，则不填写数据那么密码为空串，那么被@ModelAttribute的方法创建的对象的值会被替换为空
