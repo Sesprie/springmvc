d21665d026477f37c2acd5be818eb8ef13bad43e
sesprie <342560937@qq.com>
2019-07-23 09:19:45 +0800

    利用PUT、DELETE、GET、POST实现RESTFUL风格的CRUD操作

--- springmvc_CRUD/src/main/java/com/xixi/entity/Employee.java
+++ springmvc_CRUD/src/main/java/com/xixi/entity/Employee.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -16,4 +18,14 @@ public class Employee {
     private Integer age;
     private int sex;//female 0 male 1
     private Department department;
+    private Date birth;
+
+
+    public Employee(Integer id, String name, Integer age, int sex, Department department) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.department = department;
+    }
 }
